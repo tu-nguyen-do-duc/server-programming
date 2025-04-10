ba2204699156bc927459a19de194b6f4d4d5388e
tu-nguyen-do-duc <145394618+tu-nguyen-do-duc@users.noreply.github.com>
2025-04-10 19:52:45 +0300

    Delete bookstore directory

--- bookstore/src/main/java/fi/haagahelia/domain/Book.java
+++
@@ -1,9 +0,0 @@
-package fi.haagahelia.domain;
-
-public class Book {
-    private String title;
-    private String author;
-    private int publicationYear;
-    private String isbn;
-    private double price;
-}
